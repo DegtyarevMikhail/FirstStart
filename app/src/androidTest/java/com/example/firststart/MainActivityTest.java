67a55f7ebcd709734d6ce3ee4342626a4c7a7342
Mikhail <logindegtyarev@mail.ru>
2022-01-08 13:40:51 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/firststart/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.firststart;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testOnClick() {
+    }
+}
